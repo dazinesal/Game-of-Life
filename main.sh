d54281b23ebed2851b4140997ebc819e745225ef
Alex Antonides <alex@marxact.com>
2023-01-25 13:37:04 +0100

    Changed time to 10 minutes

--- main.sh
+++ main.sh
@@ -3,7 +3,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=16
-#SBATCH --time=00:05:00
+#SBATCH --time=00:10:00
 #SBATCH --partition=normal
 #SBATCH --output=gol_%j.out
 #SBATCH --error=gol_%j.err
